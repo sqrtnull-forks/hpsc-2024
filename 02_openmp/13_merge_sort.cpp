a289f80df6faf47778c8a64a5a4ed85314937e80
Jo Ito <sqrtnull@gmail.com>
2024-04-15 07:50:42 +0900

    mergesort

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -2,11 +2,11 @@
 #include <cstdlib>
 #include <vector>
 
-void merge(std::vector<int>& vec, int begin, int mid, int end) {
-  std::vector<int> tmp(end-begin+1);
+void merge(std::vector<int> &vec, int begin, int mid, int end) {
+  std::vector<int> tmp(end - begin + 1);
   int left = begin;
-  int right = mid+1;
-  for (int i=0; i<tmp.size(); i++) { 
+  int right = mid + 1;
+  for (int i = 0; i < tmp.size(); i++) {
     if (left > mid)
       tmp[i] = vec[right++];
     else if (right > end)
@@ -14,17 +14,20 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
     else if (vec[left] <= vec[right])
       tmp[i] = vec[left++];
     else
-      tmp[i] = vec[right++]; 
+      tmp[i] = vec[right++];
   }
-  for (int i=0; i<tmp.size(); i++) 
+  for (int i = 0; i < tmp.size(); i++)
     vec[begin++] = tmp[i];
 }
 
-void merge_sort(std::vector<int>& vec, int begin, int end) {
-  if(begin < end) {
+void merge_sort(std::vector<int> &vec, int begin, int end) {
+  if (begin < end) {
     int mid = (begin + end) / 2;
+#pragma omp task shared(vec)
     merge_sort(vec, begin, mid);
-    merge_sort(vec, mid+1, end);
+#pragma omp task shared(vec)
+    merge_sort(vec, mid + 1, end);
+#pragma omp taskwait
     merge(vec, begin, mid, end);
   }
 }
@@ -32,14 +35,16 @@ void merge_sort(std::vector<int>& vec, int begin, int end) {
 int main() {
   int n = 20;
   std::vector<int> vec(n);
-  for (int i=0; i<n; i++) {
+  for (int i = 0; i < n; i++) {
     vec[i] = rand() % (10 * n);
-    printf("%d ",vec[i]);
+    printf("%d ", vec[i]);
   }
   printf("\n");
-  merge_sort(vec, 0, n-1);
-  for (int i=0; i<n; i++) {
-    printf("%d ",vec[i]);
+#pragma omp parallel
+#pragma omp single
+  merge_sort(vec, 0, n - 1);
+  for (int i = 0; i < n; i++) {
+    printf("%d ", vec[i]);
   }
   printf("\n");
 }
