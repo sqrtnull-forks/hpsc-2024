644208f1eaad0cf0f1c2d2dc6f4563edfa9aa914
Jo Ito <sqrtnull@gmail.com>
2024-05-02 12:34:11 +0900

    fix

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <cstring>
 #include <mpi.h>
 
 struct Body {
@@ -13,7 +14,7 @@ int main(int argc, char** argv) {
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  Body ibody[N/size], jbody[N/size];
+  Body ibody[N/size], jbody[N/size], kbody[N/size];
   srand48(rank);
   for(int i=0; i<N/size; i++) {
     ibody[i].x = jbody[i].x = drand48();
@@ -26,12 +27,13 @@ int main(int argc, char** argv) {
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
+  MPI_Win win;
+  MPI_Win_create(kbody, N/size*sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
   for(int irank=0; irank<size; irank++) {
-    MPI_Win win;
-    MPI_Win_create(jbody, N/size*sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     MPI_Win_fence(0, win);
-    MPI_Put(jbody,N/size,MPI_BODY,send_to,0,N,MPI_BODY,win);
+    MPI_Put(jbody,N/size,MPI_BODY,send_to,0,N/size,MPI_BODY,win);
     MPI_Win_fence(0, win);
+    memcpy(jbody, kbody, sizeof(Body)*(N/size));
     // MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
     // MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     for(int i=0; i<N/size; i++) {
